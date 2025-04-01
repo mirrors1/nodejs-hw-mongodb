f2c6c8c76e12f60aba329efb65bfd1370af1aab6
Oleksandr Prokopchuk <sasha195@i.ua>
2025-04-01 16:04:40 +0300

    Update to throw createHttpError in controllers

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -53,8 +53,7 @@ export const deleteContactController = async (req, res, next) => {
   const contact = await deleteContact(contactId);
 
   if (!contact) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.status(204).send();
@@ -66,8 +65,7 @@ export const patchContactController = async (req, res, next) => {
   const result = await updateContact(contactId, req.body);
 
   if (!result) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.json({
