d915ef7dc7a0ba41c2b67096aec9410739e38ea2
Oleksandr Prokopchuk <sasha195@i.ua>
2025-04-05 19:34:52 +0300

    add pagination and sort

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,21 +4,20 @@ import Joi from 'joi';
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required().messages({
     'string.base': 'Username should be a string', // Кастомізація повідомлення
+    'string.empty': 'Username is not allowed to be empty',
     'string.min': 'Username should have at least {#limit} characters',
     'string.max': 'Username should have at most {#limit} characters',
     'any.required': 'Username is required',
   }),
-  phoneNumber: Joi.string().min(3).max(20).required().messages({
-    'string.base': 'PhoneNumber should be a string', // Кастомізація повідомлення
-    'string.min': 'PhoneNumber should have at least {#limit} characters',
-    'string.max': 'PhoneNumber should have at most {#limit} characters',
+  phoneNumber: Joi.string().required().messages({
+    'string.base': 'Phonenumber should be a string', // Кастомізація повідомлення
+    'string.empty': 'Phonenumber is not allowed to be empty',
     'any.required': 'PhoneNumber is required',
   }),
-  email: Joi.string().email().min(3).max(20).messages({
-    'string.base': 'Email should be a string', // Кастомізація повідомлення
-    'string.email': 'The string is not a valid email.',
-    'string.min': 'Email should have at least {#limit} characters',
-    'string.max': 'Email should have at most {#limit} characters',
+  email: Joi.string().email().messages({
+    'string.base': 'E-mail should be a string', // Кастомізація повідомлення
+    'string.empty': 'E-mail is not allowed to be empty',
+    'string.email': 'The string is not a valid e-mail.',
   }),
   isFavourite: Joi.boolean().required().messages({
     'boolean.base': 'IsFavourite should be a boolean', // Кастомізація повідомлення
@@ -26,17 +25,11 @@ export const createContactSchema = Joi.object({
   }),
   contactType: Joi.string()
     .valid('work', 'home', 'personal')
-    .min(3)
-    .max(20)
     .required()
     .messages({
-      'string.base': 'ContactType should be a string', // Кастомізація повідомлення
-      'string.empty': 'ContactType is not allowed to be empty', // Кастомізація повідомлення
-      'string.min': 'ContactType should have at least {#limit} characters',
-      'string.max': 'ContactType should have at most {#limit} characters',
+      'any.only':
+        'СontactType only allows values: `work`, `home` or `personal`', // Кастомізація повідомлення
       'any.required': 'ContactType is required',
-      'any.valid':
-        'СontactType only allows values "work", "home" or "personal"',
     }),
 });
 
@@ -47,29 +40,19 @@ export const updateContactSchema = Joi.object({
     'string.min': 'Username should have at least {#limit} characters',
     'string.max': 'Username should have at most {#limit} characters',
   }),
-  phoneNumber: Joi.string().min(3).max(20).messages({
-    'string.base': 'PhoneNumber should be a string', // Кастомізація повідомлення
-    'string.min': 'PhoneNumber should have at least {#limit} characters',
-    'string.max': 'PhoneNumber should have at most {#limit} characters',
+  phoneNumber: Joi.string().messages({
+    'string.base': 'Phonenumber should be a string', // Кастомізація повідомлення
+    'string.empty': 'Phonenumber is not allowed to be empty',
   }),
-  email: Joi.string().email().min(3).max(20).messages({
-    'string.base': 'Email should be a string', // Кастомізація повідомлення
-    'string.email': 'The string is not a valid email.',
-    'string.min': 'Email should have at least {#limit} characters',
-    'string.max': 'Email should have at most {#limit} characters',
+  email: Joi.string().email().messages({
+    'string.base': 'E-mail should be a string', // Кастомізація повідомлення
+    'string.empty': 'E-mail is not allowed to be empty',
+    'string.email': 'The string is not a valid e-mail.',
   }),
   isFavourite: Joi.boolean().messages({
     'boolean.base': 'IsFavourite should be a boolean', // Кастомізація повідомлення
   }),
-  contactType: Joi.string()
-    .valid('work', 'home', 'personal')
-    .min(3)
-    .max(20)
-    .messages({
-      'string.base': 'ContactType should be a string', // Кастомізація повідомлення
-      'string.min': 'ContactType should have at least {#limit} characters',
-      'string.max': 'ContactType should have at most {#limit} characters',
-      'any.valid':
-        'СontactType only allows values "work", "home" or "personal"',
-    }),
+  contactType: Joi.string().valid('work', 'home', 'personal').messages({
+    'any.only': 'СontactType only allows values: `work`, `home` or `personal`', // Кастомізація повідомлення
+  }),
 });
