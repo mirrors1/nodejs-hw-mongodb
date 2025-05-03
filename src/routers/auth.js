451bf2ea6646f17fc3f95fc0269b50655fb65793
Oleksandr Prokopchuk <sasha195@i.ua>
2025-05-03 09:01:07 +0300

    Renamed POST routes /auth/reset-password to /auth/reset-pwd and /request-reset-email to /send-reset-email

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -35,13 +35,13 @@ authRouter.post('/logout', ctrlWrapper(logoutUserController));
 authRouter.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
 authRouter.post(
-  '/request-reset-email',
+  '/send-reset-email',
   validateBody(requestResetEmailSchema),
   ctrlWrapper(requestResetEmailController),
 );
 
 authRouter.post(
-  '/reset-password',
+  '/reset-pwd',
   validateBody(resetPasswordSchema),
   ctrlWrapper(resetPasswordController),
 );
